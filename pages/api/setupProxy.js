fd81f486d60514b364ec8f809973a27e5ee166d6
Sandeep Maheshwari <sandeep_maheshwari@epam.com>
2023-02-02 10:36:30 +0000

    setupProxy commented

--- pages/api/setupProxy.js
+++ pages/api/setupProxy.js
@@ -1,33 +1,33 @@
-/*
-Copyright 2020 Adobe
-All Rights Reserved.
+// /*
+// Copyright 2020 Adobe
+// All Rights Reserved.
 
-NOTICE: Adobe permits you to use, modify, and distribute this file in
-accordance with the terms of the Adobe license agreement accompanying
-it.
-*/
-const { SERVICE_TOKEN, DEV_TOKEN, BASIC } = require('../src/proxy/authMethods');
+// NOTICE: Adobe permits you to use, modify, and distribute this file in
+// accordance with the terms of the Adobe license agreement accompanying
+// it.
+// */
+// const { SERVICE_TOKEN, DEV_TOKEN, BASIC } = require('../src/proxy/authMethods');
 
-const proxy = (() => {
-    switch (process.env.REACT_APP_AUTH_METHOD) {
-      case SERVICE_TOKEN:
-        // Use Service token exchange for Cloud Env PROD
-        return require('../src/proxy/setupProxy.auth.service-token');
-      case DEV_TOKEN:
-        // Use Dev token for local development with Cloud Env
-        return require('../src/proxy/setupProxy.auth.dev-token');
-      case BASIC:
-        // Use user/pass for local development with Local Author Env
-        return require('../src/proxy/setupProxy.auth.basic');
-      default:
-        // Auth not needed for local development with Local Publisher Env
-        return require('../src/proxy/setupProxy.auth.none');
-    }
-})();
+// const proxy = (() => {
+//     switch (process.env.REACT_APP_AUTH_METHOD) {
+//       case SERVICE_TOKEN:
+//         // Use Service token exchange for Cloud Env PROD
+//         return require('../src/proxy/setupProxy.auth.service-token');
+//       case DEV_TOKEN:
+//         // Use Dev token for local development with Cloud Env
+//         return require('../src/proxy/setupProxy.auth.dev-token');
+//       case BASIC:
+//         // Use user/pass for local development with Local Author Env
+//         return require('../src/proxy/setupProxy.auth.basic');
+//       default:
+//         // Auth not needed for local development with Local Publisher Env
+//         return require('../src/proxy/setupProxy.auth.none');
+//     }
+// })();
 
-/*
-    Set up a proxy with AEM for local development
-    In a production environment this proxy should be set up at the webserver level or absolute URLs should be used.
-*/
+// /*
+//     Set up a proxy with AEM for local development
+//     In a production environment this proxy should be set up at the webserver level or absolute URLs should be used.
+// */
 
-module.exports = proxy;
+// module.exports = proxy;
