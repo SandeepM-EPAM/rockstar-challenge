987c156e1b1910f6cf27aac27a4817bc82a93a8b
Sandeep Maheshwari <sandeep_maheshwari@epam.com>
2023-02-07 12:48:43 +0000

    added launch script

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,8 @@ export default function NextApp({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
-    <>   
+    <>
+      <script src="https://assets.adobedtm.com/4ef014d90ea4/8f46d152dc3b/launch-534dbc3f5ac1-development.min.js" async></script>
       <ApolloProvider client={apolloClient}>
         <App>
           <Component {...pageProps} />
